34df8c9e3464818361b2520e80d5da443dd9aafc
Andriy Kalysh <53571019+mad-ben@users.noreply.github.com>
2022-12-27 09:57:27 +0000

    Some updates

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.post('/', async (req,res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0.5,
+            temperature: 0.8,
             max_tokens: 4000,
             top_p: 1,
             frequency_penalty: 0.5,
