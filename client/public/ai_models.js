5a1b5478145499df462f245c0d6324cebdc8095f
Andriy Kalysh <53571019+mad-ben@users.noreply.github.com>
2022-12-30 11:53:26 +0000

    Update ai_models.js

--- client/public/ai_models.js
+++ client/public/ai_models.js
@@ -13,7 +13,7 @@ export const aiModel1 = {
 export const aiModel2 = {
     model: "code-davinci-002",
     temperature: 0,
-    max_tokens: 100,
+    max_tokens: 256,
     top_p:1,
     frequency_penalty: 0,
     presence_penalty: 0,
